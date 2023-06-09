f22bc296363e8c83dd4802e2682fa07905bfcbf8
Carlos Sirias <carlos.sirias04@gmail.com>
2023-06-08 17:42:41 -0700

    :alien: Comments added!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 
+//Event listenes to hear every time the user scroll.
 window.addEventListener("scroll", function(){
+    //Make variables to header and the navbar, because the header will be able to be sticky and the navbar to change the color.
     var navHeader = document.querySelector("header");
     var nav = this.document.querySelector("nav");
 
+    //To toggle classlist everytime the scrollY be greather than 0
     navHeader.classList.toggle("header__navbar__sticky", window.scrollY > 0);
     nav.classList.toggle("nav--active", this.window.scrollY > 0);
 })
--- js/type.js
+++ js/type.js
@@ -1,8 +1,10 @@
+//This object is used to control the velocity, delay and loop of the type
+//This has attributes to change this parameters.
 const type = new Typed('.text__main__coreword', {
-    strings: ['Developer', 'Coder', 'Student'],
-    loop: true,
-    typeSpeed: 80,
-    backSpeed: 80,
-    startDelay: 300,
-    loopCount: false,
+    strings: ['Developer', 'Coder', 'Student'],//words that you wanna show
+    loop: true,//If you want to be a loop
+    typeSpeed: 80,//Type speed
+    backSpeed: 80,//Back Type speed
+    startDelay: 300,//If u want a delay at start
+    loopCount: false,//How many times to repeat the array
 });
